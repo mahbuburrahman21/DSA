db267464b28567679b502cb3c10161b8a06c1e68
mahbuburrahman21 <mahbuburrahman2u5t@gmail.com>
2025-12-25 15:29:58 +0600

    init

--- src/LeetCodeSolution/PlusOne_66.java
+++ src/LeetCodeSolution/PlusOne_66.java
@@ -1,33 +1,28 @@
 package LeetCodeSolution;
+import Arrays.ArrayUtility;
+
 import java.util.ArrayList;
 public class PlusOne_66 {
     static void main(String[] args) {
-        int[] digits = {9,9,8,7};
-        plusOne(digits);
+        //int[] digits = {1,2,3};
+        //int[] digits = {9,9,9};
+        //int[] digits = {9,9,9,9};
+        int[] digits = {9,8,7,6,5,4,3,2,1,0};
+        int[] ar = plusOne(digits);
+        //ArrayUtility.displayArray(ar);
     }
     public static int[] plusOne(int[] digits) {
-
-        int n = digits.length; // n = 3
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += (int) Math.pow(10, n-1-i)*digits[i];
+        int n = digits.length;
+        for (int i = n-1; i >= 0; i--) {
+            if (digits[i]<9){
+                digits[i]++; // no carry needed
+                return digits;
+            }
+            digits[i] = 0;
         }
-        sum++;
-
-        int size = 10;
-        int[] newArr = new int[size];
-
-        for (int i = 0; i < n; i++) {
-
-        }
-
-        System.out.println(sum);
-        return null;
+        //If all digits were 9 -> new array with leading 1
+        int[] newArr = new int[n+1];
+        newArr[0] = 1;
+        return newArr;
     }
 }
-
-
-
-//            System.out.printf("%d * 10^%d = %d\n", digits[i], n-1-i,(int) Math.pow(10, n-1-i)*digits[i]);
-//            System.out.println(sum);
-//            System.out.println();
