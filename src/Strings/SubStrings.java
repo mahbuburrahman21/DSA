1640b1953a30d6c6ffd9f283bc7471237eea6177
mahbuburrahman21 <mahbuburrahman2u5t@gmail.com>
2026-01-09 18:21:04 +0600

    init

---
+++ src/Strings/SubStrings.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class SubStrings {
+    static void main(String[] args) {
+        String str = "Shokuntola";
+        System.out.println(str.substring(3));
+        System.out.println(str.substring(3,6));
+    }
+}
--- src/Strings/SubStringsOfString.java
+++
@@ -1,7 +0,0 @@
-package Strings;
-
-public class SubStringsOfString {
-    static void main(String[] args) {
-
-    }
-}
