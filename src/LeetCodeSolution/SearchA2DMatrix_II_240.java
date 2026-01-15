9eadc940e1ebed97ec25b95d8e60ccead818fa05
mahbuburrahman21 <mahbuburrahman2u5t@gmail.com>
2026-01-15 13:43:25 +0600

    init

--- src/LeetCodeSolution/SearchA2DMatrix_II_240.java
+++ src/LeetCodeSolution/SearchA2DMatrix_II_240.java
@@ -2,35 +2,27 @@ package LeetCodeSolution;
 
 public class SearchA2DMatrix_II_240 {
     static void main(String[] args) {
-        int[][] matrix = {{1,3,5,7},{10,11,16,20},{23,30,34,60},{70,71,74,76}};
+        int[][] matrix = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
         //int[][] matrix = {{1,4},{2,5}};
         //int potentialRow = findingPotentialRow(matrix, 20);
         //System.out.println(potentialRow);
-        boolean isFound = searchMatrix(matrix,  30);
+        boolean isFound = searchMatrix(matrix,  33);
         if (isFound) System.out.println("Target found!");
         else System.out.println("Target not found!");
     }
 
 
-    public static boolean searchMatrix(int[][] matrix, int target){
-        int row = 0;
-        int loop = matrix.length;
-
-        for (int i = 0; i < loop; i++) {
-            int start = 0;
-            int end = matrix[0].length-1;
-            while (start <= end){
-                int mid = start+(end-start)/2; // mid is the col number here.
-
-                if (matrix[row][mid] == target) return true;
-                else if (matrix[row][mid] < target) start = mid+1;
-                else if (matrix[row][mid] > target) end = mid-1;
-            }
-            row++;
-            System.out.println("Not found");
+    public static boolean searchMatrix(int[][] arr, int target){
+        int row = arr.length;
+        int col = arr[0].length;
+        int i = 0;
+        int j = col-1;
+        while (i < row && j >= 0){
+            //System.out.println("Element which compared: "+arr[i][j]);
+            if (arr[i][j] == target) return true;
+            else if (arr[i][j] > target) j--; //move left
+            else i++; //move down
         }
-
         return false;
     }
-    //Total Time Complexity = O(mlog(n))
 }
