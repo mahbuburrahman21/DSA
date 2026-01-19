1d02086907ffa0d40f359dc1043335cab7987403
mahbuburrahman21 <mahbuburrahman2u5t@gmail.com>
2026-01-19 13:57:14 +0600

    init

--- src/LeetCodeSolution/ClimbingStairs_70.java
+++ src/LeetCodeSolution/ClimbingStairs_70.java
@@ -4,9 +4,24 @@ public class ClimbingStairs_70 {
     static void main() {
         System.out.println(climbStairs(40));
     }
-    public static int climbStairs(int n) { // Gives Time limit exceed (TLE) error.
+
+    // Gives Time limit exceed ----------(TLE) error----------.
+    //public static int climbStairs(int n) {
+        //if(n==1) return 1;
+        //if(n==2) return 2;
+        //return climbStairs(n-1) + climbStairs(n-2);
+    //}
+
+
+    //Optimized program using dynamic programing--------(DP)---------.
+    public static int climbStairs(int n) {
         if(n==1) return 1;
-        if(n==2) return 2;
-        return climbStairs(n-1) + climbStairs(n-2);
+        int[] arr = new int[n+1];
+        arr[1] = 1;
+        arr[2] = 2;
+        for(int i = 3; i <= n; i++){
+            arr[i] = arr[i-1]+arr[i-2];
+        }
+        return arr[n];
     }
 }
