e25589a930b0b5b36619f0eb07f255ba6af8e470
mahbuburrahman21 <mahbuburrahman2u5t@gmail.com>
2026-01-20 14:05:56 +0600

    init

--- src/Recursion/ReverseArrayUsingRecursion.java
+++ src/Recursion/ReverseArrayUsingRecursion.java
@@ -6,12 +6,13 @@ public class ReverseArrayUsingRecursion {
     static void main() {
         int[] arr = {1,2,3,4,5,6,7,8,9,10};
         int n = arr.length-1;
+        System.out.println(Arrays.toString(arr));
         System.out.println(Arrays.toString(revArray(arr,0, n)));
         System.out.println(Arrays.toString(arr));
     }
 
     private static int[] revArray(int[] arr, int start, int end){
-        if (start<end) return arr;
+        if (start >= end) return arr;
         swap(arr,start,end);
         return revArray(arr, start+1, end-1);
     }
