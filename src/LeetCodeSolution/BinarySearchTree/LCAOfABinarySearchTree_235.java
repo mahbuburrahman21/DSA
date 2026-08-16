d646a0d1f3c68b002296ad0159559cb93b679d62
mahbuburrahman21 <YOUR_GITHUB_NOREPLY_EMAIL>
2026-08-16 17:05:22 +0600

    Commit check

--- src/LeetCodeSolution/BinarySearchTree/LCAOfABinarySearchTree_235.java
+++ src/LeetCodeSolution/BinarySearchTree/LCAOfABinarySearchTree_235.java
@@ -2,6 +2,7 @@ package LeetCodeSolution.BinarySearchTree;
 
 public class LCAOfABinarySearchTree_235 {
     // Recursive solution with O(h) stack space
+    // Time complexity is O(h) ----> h is the tree height here
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if(root.val < p.val && root.val < q.val)
             return lowestCommonAncestor(root.right, p, q);
